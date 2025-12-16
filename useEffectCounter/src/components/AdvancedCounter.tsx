b85319a999913ba5135177c08cec6e46cbdeda9c
4drea4 <bakerand@outlook.com>
2025-12-15 18:22:47 -0600

    working on saving popup

--- useEffectCounter/src/components/AdvancedCounter.tsx
+++ useEffectCounter/src/components/AdvancedCounter.tsx
@@ -4,14 +4,22 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState<number[]>([0]);
   const [inputText, setInputText] = useState(1);
-  const [isSaving, setSaving] = useState(false);
+  const [isSaving, setSaving] = useState("");
+  const [isSaved, setSaved] = useState("");
+
+
 
   //Assistance from Patrice
   useEffect(() => {
     localStorage.setItem("countKey" , JSON.stringify(count))
     console.log('ravioli');
-    setTimeout(()=>{setSaving(false)} , 2000)
-  }, [count]);//watching count
+    setTimeout(()=>{setSaving("saving...")} , 2000)
+    setTimeout(()=> {setSaving("")
+      setSaved("saved...") , 1500
+    }
+  )
+
+  }, [count, isSaving ]);//watching count
   
   const reset = () =>{
     setCount(0)
@@ -20,7 +28,7 @@ export default function Counter() {
   //events
 
   const handleClick = () => {
-    setSaving(true); //saving but need to turn it false intermitently.
+    setSaving("Saving..."); //saving but need to turn it false intermitently.
     setCount((prevCount) => {
       const spaghetti = prevCount + inputText
       setHistory(history => [...history,spaghetti] );//no stale use the newest verstion of history
@@ -28,7 +36,7 @@ export default function Counter() {
 }) } ;
 
   const handleDecrementClick =() => {
-    setSaving(true); //saving but need to turn it false intermitently.
+    setSaving("Saving..."); //saving but need to turn it false intermitently.
     setCount((prevCount) => {
       const rigatoni = prevCount - inputText
       setHistory(history => [...history,rigatoni] ); //history is the name of the array of numbers and adding rigatoni to the end of the array of numbers
@@ -59,8 +67,8 @@ export default function Counter() {
           <li key={index}>{num}</li>
         ))}
       </ul>
-          {isSaving &&   <div>Saving...</div>}
-    
+          {isSaving ?    <div> </div>: <div>{isSaving}</div>} 
+    {/* is saving truthy or falsy no? then display saving... */}
       
           </div>
     </div>
