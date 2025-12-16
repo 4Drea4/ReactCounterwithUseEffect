9631fccf16b6debaeed46d8eb56ef5d2929b6dbe
4drea4 <bakerand@outlook.com>
2025-12-15 18:01:32 -0600

    Patrice is the best, use effect with settimeout

--- useEffectCounter/src/components/AdvancedCounter.tsx
+++ useEffectCounter/src/components/AdvancedCounter.tsx
@@ -4,19 +4,23 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState<number[]>([0]);
   const [inputText, setInputText] = useState(1);
+  const [isSaving, setSaving] = useState(false);
 
   //Assistance from Patrice
   useEffect(() => {
     localStorage.setItem("countKey" , JSON.stringify(count))
     console.log('ravioli');
-  }, [count]);
+    setTimeout(()=>{setSaving(false)} , 2000)
+  }, [count]);//watching count
   
   const reset = () =>{
     setCount(0)
     setHistory([])
   }
+  //events
 
   const handleClick = () => {
+    setSaving(true); //saving but need to turn it false intermitently.
     setCount((prevCount) => {
       const spaghetti = prevCount + inputText
       setHistory(history => [...history,spaghetti] );//no stale use the newest verstion of history
@@ -24,6 +28,7 @@ export default function Counter() {
 }) } ;
 
   const handleDecrementClick =() => {
+    setSaving(true); //saving but need to turn it false intermitently.
     setCount((prevCount) => {
       const rigatoni = prevCount - inputText
       setHistory(history => [...history,rigatoni] ); //history is the name of the array of numbers and adding rigatoni to the end of the array of numbers
@@ -54,6 +59,8 @@ export default function Counter() {
           <li key={index}>{num}</li>
         ))}
       </ul>
+          {isSaving &&   <div>Saving...</div>}
+    
       
           </div>
     </div>
